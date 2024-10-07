14e3ae9d12d3147d96ad392442ed3beaf1898b87
tlenoble <tlenoble@gingerminds.fr>
2024-10-07 15:20:41 +0200

    fix(Symfony) change http client from guzzle to symfony

--- src/MessageHandler/FetchProductAvisVerifiesHandler.php
+++ src/MessageHandler/FetchProductAvisVerifiesHandler.php
@@ -33,41 +33,43 @@ class FetchProductAvisVerifiesHandler implements MessageHandlerInterface
         ]);
         $channel = $this->channelRepository->find($message->getChannelId());
         
-        foreach ($this->webservice->fetchProductReviews($product, $channel) as $arr) {
-            $review = $this->em->getRepository(AvisVerifiesProductReviewInterface::class)->findOneBy([
-                'idSource' => $arr['id_review_product']
-            ]);
-
-            if ($review === null) {
-                $review = $this->productReviewFactory->createNew();
-                
-                $product = $this->em->getRepository(ProductInterface::class)->findOneBy([
-                    'code' => $arr['id_product'],
+        if (null !== $product) {
+            foreach ($this->webservice->fetchProductReviews($product, $channel) as $arr) {
+                $review = $this->em->getRepository(AvisVerifiesProductReviewInterface::class)->findOneBy([
+                    'idSource' => $arr['id_review_product']
                 ]);
 
-                $review->setReviewSubject($product);
-                $review->setSource('avisverifies');
-                $review->setIdSource($arr['id_review_product']);
-                $review->setCreatedAt(new \DateTime($arr['review_date']));
+                if ($review === null) {
+                    $review = $this->productReviewFactory->createNew();
+
+                    $product = $this->em->getRepository(ProductInterface::class)->findOneBy([
+                        'code' => $arr['id_product'],
+                    ]);
+
+                    $review->setReviewSubject($product);
+                    $review->setSource('avisverifies');
+                    $review->setIdSource($arr['id_review_product']);
+                    $review->setCreatedAt(new \DateTime($arr['review_date']));
+                }
+
+                $review->setComment($arr['review']);
+                $review->setRating((int)$arr['rate']);
+                $review->setRawSource($arr);
+                $review->setStatus('accepted');
+
+                $order = $this->em->getRepository(Order::class)->findOneBy([
+                    'number' => $arr['order_ref'],
+                    'channel' => $channel
+                ]);
+
+                if ($order instanceof OrderInterface) {
+                    $review->setAuthor($order->getCustomer());
+
+                    $this->em->persist($review);
+                    $this->em->flush();
+                }
+
             }
-
-            $review->setComment($arr['review']);
-            $review->setRating((int)$arr['rate']);
-            $review->setRawSource($arr);
-            $review->setStatus('accepted');
-
-            $order = $this->em->getRepository(Order::class)->findOneBy([
-                'number' => $arr['order_ref'],
-                'channel' => $channel
-            ]);
-
-            if ($order instanceof OrderInterface) {
-                $review->setAuthor($order->getCustomer());
-
-                $this->em->persist($review);
-                $this->em->flush();
-            }
-            
         }
 
     }
